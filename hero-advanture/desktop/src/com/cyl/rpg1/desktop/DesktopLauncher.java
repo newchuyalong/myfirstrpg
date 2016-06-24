0c9680c2cb1b3b46e78568c90ea156995ae05d34
newchuyalong <newchuyalong@outlook.com>
2016-06-24 04:23:18 +0000

    Add files via upload

---
+++ hero-advanture/desktop/src/com/cyl/rpg1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cyl.rpg1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cyl.rpg1.HeroGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HeroGame(), config);
+	}
+}
